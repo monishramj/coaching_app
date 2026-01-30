b5b4dffb823e398f2d7c88ca9c7061a0f8bae4c6
wikzAM <anubhav.anuboss@gmail.com>
2026-01-30 04:25:33 -0500

    After monish's pull, I fixed merging issues and cleaned up some things with authentication.  Not tested if it runs with the current files, so I will see and first push.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,9 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 import { AppState } from 'react-native';
 import 'react-native-url-polyfill/auto';
 
-// Ask Adarsh for these or find them in Supabase -> Project Settings -> API
 const supabaseUrl = 'https://fayxjdvnrwcqdsfsorof.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZheXhqZHZucndjcWRzZnNvcm9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3MDg5NzUsImV4cCI6MjA4NTI4NDk3NX0.sGbQqJFgarT3pANPtd8wm1m0lQ6YaDKLN7pOrWO9pc8';
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
